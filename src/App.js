63202b82fa30baa0d606b31b2ffb94cc42aa2dcc
Sen Wang <wangsen@caicloud.io>
2018-10-11 11:45:16 +0800

    feat(app): 修改 app.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ const Topics = () => "Topics";
 const A = () => "A";
 const B = () => "B";
 const C = () => "C";
+const Hello = () => "Hello";
 
 class Button extends Component {
   render() {
@@ -21,56 +22,57 @@ class App extends React.Component {
     return (
       <div>
         <div>HistoryRouter</div>
-        <hr />
-        <ul className="nav">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/topics">Topics</Link>
-          </li>
-        </ul>
-        <Button to="/">Home</Button>
-        <Button to="/about">About</Button>
-        <Button to="/topics">Topics</Button>
-        <button
-          onClick={() => {
-            window.history.pushState(
-              { someState: "hello" },
-              null,
-              window.location.pathname
-            );
-            this.forceUpdate();
-          }}
-        >
-          push-with-state
-        </button>
-        <hr />
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/topics" component={Topics} />
-        {JSON.stringify(window.history.state)}
-        <hr style={{ marginTop: 200 }} />
-        <div>hashRouter</div>
+        <div>
+          <ul className="nav">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/topics">Topics</Link>
+            </li>
+          </ul>
+          <Button to="/">Home</Button>
+          <Button to="/about">About</Button>
+          <Button to="/topics">Topics</Button>
+          <button
+            onClick={() => {
+              window.history.pushState({ someState: "hello" }, null, "/hello");
+              this.forceUpdate();
+            }}
+          >
+            push-with-state
+          </button>
+          <div>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/topics" component={Topics} />
+            <Route path="/hello" component={Hello} />
+          </div>
+          <div>{JSON.stringify(window.history.state || {})}</div>
+        </div>
         <hr />
 
-        <ul className="nav">
-          <li>
-            <HashLink to="/a">a</HashLink>
-          </li>
-          <li>
-            <HashLink to="/b">b</HashLink>
-          </li>
-          <li>
-            <HashLink to="/c">c</HashLink>
-          </li>
-        </ul>
-        <HashRoute path="/a" component={A} />
-        <HashRoute path="/b" component={B} />
-        <HashRoute path="/c" component={C} />
+        <div>hashRouter</div>
+        <div>
+          <ul className="nav">
+            <li>
+              <HashLink to="/a">a</HashLink>
+            </li>
+            <li>
+              <HashLink to="/b">b</HashLink>
+            </li>
+            <li>
+              <HashLink to="/c">c</HashLink>
+            </li>
+          </ul>
+
+          <HashRoute path="/a" component={A} />
+          <HashRoute path="/b" component={B} />
+          <HashRoute path="/c" component={C} />
+        </div>
       </div>
     );
   }
